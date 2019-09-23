4ed056907d4fb757827da08836a8ffe9399dbd1b
salEZY <bobicaleks@yahoo.com>
2019-09-23 11:33:19 +0200

    add passport to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const connectDb = require('./utils/db')
 
@@ -9,6 +10,8 @@ connectDb()
 app.use(express.json({ extended: false }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/user', require('./routes/user'))
 app.use('/google', require('./routes/google'))
